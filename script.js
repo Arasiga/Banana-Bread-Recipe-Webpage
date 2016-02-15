c9130d0b7a3a2fe5ba9bf331a1d93bf8c4950ec2
Alex Rasiga <alex.rasiga@gmail.com>
2016-02-15 14:11:00 -0500

    HTML file added

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	console.log("script included!");
+
+	$('li').click(function(){
+		$(this).remove();
+	});
+
+  $('p').click(function(){
+    $('img').toggle("slow", function(){
+     
+    });
+  });
+ //  $('p').toggle(
+	// $('p').click(function(){
+	// 	$('img').remove();
+	// });
+
+ //  $('p').dblclick(function(){
+ //    $('img').add();
+ //  });
+ //  )
+
+});
+
+// $(document).ready(function(){
+// 	$('li').click(function(){
+// 		$(this).remove();
+// 	});
+// });
+
+// $(document).ready(function(){
+// 	$('p').click(function(){
+// 		$('img').remove();
+// 	});
+// });
